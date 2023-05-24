8004cd08d70aeac45baa15c7938ec3f3e49f7245
freelancernizamc <nizamuddin2020ctg@gmail.com>
2023-05-24 14:51:53 +0600

    menu page completed

---
+++ src/pages/Menu/MenuCategory/MenuCategory.jsx
@@ -0,0 +1,21 @@
+import Cover from "../../Shared/Cover/Cover";
+import MenuItem from "../../Shared/MenuItem/MenuItem";
+
+
+const MenuCategory = ({ items, title, img }) => {
+    return (
+        <div className="pt-8">
+            {title && <Cover img={img} title={title} />}
+            <div className="grid md:grid-cols-2 gap-10 my-16">
+                {
+                    items.map(item => <MenuItem
+                        key={item._id}
+                        item={item}
+                    ></MenuItem>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default MenuCategory;
